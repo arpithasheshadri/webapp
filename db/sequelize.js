339abcd30d85d104b368715a6e47b6c795e9c1cb
Arpitha Sheshadri Bhat <144745793+arpithasheshadri@users.noreply.github.com>
2024-02-15 10:04:39 -0500

    modified dialect in sequelize

--- db/sequelize.js
+++ db/sequelize.js
@@ -16,7 +16,7 @@ dotenv.config({
 
 const sequelize = new Sequelize(process.env.DATABASE, process.env.USER, process.env.PASSWORD, {
   host: process.env.HOST,
-  dialect: process.env.DATABASE_SYS,
+  dialect: 'postgres',
   logging: false
 });
 
