2c0a43ce97e24b92725974789879ee780f406685
Arpitha Sheshadri Bhat <144745793+arpithasheshadri@users.noreply.github.com>
2024-02-28 23:00:25 -0500

    modified workflow to install postgres

--- postgres_setup.sh
+++ postgres_setup.sh
@@ -21,17 +21,18 @@ sudo systemctl enable postgresql
 sudo -u postgres psql -c "CREATE USER $USER WITH PASSWORD '$PASSWORD';" 2>/dev/null
 sudo -u postgres psql -c "CREATE DATABASE $USER OWNER $USER;" 2>/dev/null
 sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE $USER TO $USER;" 2>/dev/null
+sudo sed -i "s/ident/md5/g" /var/lib/pgsql/data/pg_hba.conf
 # Find and edit pg_hba.conf
-PG_HBA_CONF=$(sudo find / -name "pg_hba.conf" 2>/dev/null)
-if [ -n "$PG_HBA_CONF" ]; then
-    sudo sed -i "s/ident/md5/g" "$PG_HBA_CONF"
-    # Add configuration to allow connections from any IP address with password authentication
-    echo "host    all             all             0.0.0.0/0               md5" | sudo tee -a "$PG_HBA_CONF" >/dev/null
-    # Restart PostgreSQL service for changes to take effect
-    sudo systemctl restart postgresql
-else
-    echo "pg_hba.conf not found."
-fi
+# PG_HBA_CONF=$(sudo find / -name "pg_hba.conf" 2>/dev/null)
+# if [ -n "$PG_HBA_CONF" ]; then
+#     sudo sed -i "s/ident/md5/g" "$PG_HBA_CONF"
+#     # Add configuration to allow connections from any IP address with password authentication
+#     echo "host    all             all             0.0.0.0/0               md5" | sudo tee -a "$PG_HBA_CONF" >/dev/null
+#     # Restart PostgreSQL service for changes to take effect
+#     sudo systemctl restart postgresql
+# else
+#     echo "pg_hba.conf not found."
+# fi
 # Configure PostgreSQL to allow connections from external hosts (if needed)
 # Edit the PostgreSQL configuration file: /var/lib/pgsql/data/pg_hba.conf
 # Add the following line to allow connections from any IP address with password authentication:
