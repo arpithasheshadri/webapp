14e4a2821076e6dec185e5db1a8cef5d4c40a26c
Arpitha Sheshadri Bhat <144745793+arpithasheshadri@users.noreply.github.com>
2024-02-29 00:00:47 -0500

    modified integration test file

--- test/integration-test.test.js
+++ test/integration-test.test.js
@@ -5,11 +5,6 @@ import sequelize from '../db/sequelize.js';
 
 const request = supertest(app);
 
-before(async () => {
-  // Wait for the database synchronization to complete before running tests
-  await sequelize.sync({ force: false, alter:true });
-});
-
 describe('User /v1/user API Integration Tests', () => {
 
   // Test 1: Create an account and validate it exists
