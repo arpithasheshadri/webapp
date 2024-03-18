c39f353d1696ec3b3aad8b855cff3d5770ab7d85
Arpitha Sheshadri Bhat <144745793+arpithasheshadri@users.noreply.github.com>
2024-03-17 23:49:06 -0400

    added code related to json logging

--- controllers/healthcheck-controller.js
+++ controllers/healthcheck-controller.js
@@ -1,22 +1,29 @@
 import { checkConnection } from "../services/healthcheck-service.js";
 import { setResponse, setErrorResponse } from "./response-handler.js";
+import logger from "./logger.js";
 
 export const healthCheck = async (request, response) => {
+    logger.info("Healthcheck started");
     response.set('Cache-Control', 'no-cache');
     try {
         if(Object.keys(request.body).length != 0 || Object.keys(request.query).length != 0){
+            logger.error("request body (or) request parameter should not be sent");
             response.status(400).send();
         } else {
             await checkConnection();
+            logger.info("Healthcheck successful");
+            logger.info("Healthcheck completed");
             response.status(200).send();
         }
     } catch (err) {
+        logger.error(`Healthcheck Failed : ${err}`);
         setErrorResponse(503, err, response);
     }
 }
 
 export const methodCheck = async (request, response) => {
     response.set('Cache-Control', 'no-cache');
+    logger.error(`Healthcheck Failed : Requested method not allowed`);
     response.status(405).send();
 }
 
