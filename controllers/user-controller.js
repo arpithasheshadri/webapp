a6c46d9654e2780f056c925f127ff3cca3d9c3bc
Arpitha Sheshadri Bhat <144745793+arpithasheshadri@users.noreply.github.com>
2024-03-17 23:49:06 -0400

    added code related to json logging

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -4,12 +4,14 @@ import { v4 as uuidv4 } from "uuid";
 import bcrypt from "bcrypt";
 import { authentication } from "./authentication.js";
 import { validate } from "./validation.js";
+import logger from "./logger.js";
 
 export const createUser = async (request, response) => {
+    logger.info("User creation started");
     try {
         const isValid = validate(request.body);
-        console.log("inside first try",request.body);
         if (!isValid.isValid) {
+            logger.error(`Error occured while creating user : ${isValid.errors.message}`);
             setErrorResponse(isValid.errors?.code, isValid.errors,response)
         } else {
             const salt = await bcrypt.genSalt(10);
@@ -22,9 +24,7 @@ export const createUser = async (request, response) => {
                 first_name: request.body.first_name,
                 last_name: request.body.last_name
             }
-            console.log("created user body",user);
             const addedUser = await addUser(user);
-            console.log("response",addedUser);
             let responseUser = {
                 id: addedUser.id,
                 username: addedUser.username,
@@ -34,24 +34,29 @@ export const createUser = async (request, response) => {
                 account_updated: addedUser.account_updated
 
             }
+            logger.info("User creation successful");
             response.status(201).send(responseUser);
         }
     } catch (err) {
         if (err.name === 'SequelizeUniqueConstraintError') {
+            logger.error(`Error occured while creating user : Username (or) Email already exists`);
             setErrorResponse(400, "Username (or) Email already exists", response);
         } else {
-            console.error(err);
+            logger.error(`Error occured while creating user : ${err}`);
             setErrorResponse(503, err, response);
         }
     }
 }
 
 export const getUser = async (request, response) => {
+    logger.info("Fetching user started");
     try {
         const auth = request.headers.authorization;
         if (!auth || Object.keys(auth).length === 0) {
+            logger.error(`Error occured while fetching user : Authentication credentials required`);
             setErrorResponse(400, "Authentication credentials required", response);
         } else if(Object.keys(request.body).length != 0 || Object.keys(request.query).length != 0) {
+            logger.error(`Error occured while fetching user : Request body should not be sent`);
             setErrorResponse(400, "Request body should not be sent", response);
         } else {
             const authenticatedUser = await authentication(auth);
@@ -64,17 +69,21 @@ export const getUser = async (request, response) => {
                     account_created: authenticatedUser.account_created,
                     account_updated: authenticatedUser.account_updated
                 }
+                logger.info(`User fetched successfully`);
                 setResponse(user, response);
             } else {
+                logger.error(`Error occured while fetching user : Invalid credentials provided`);
                 setErrorResponse(401, "Invalid credentials provided", response);
             }
         }
     } catch (err) {
+        logger.error(`Error occured while fetching user : ${err}`);
         setErrorResponse(503, err, response);
     }
 }
 
 export const updateUser = async (request, response) => {
+    logger.info("Updating user started");
     response.set('Cache-Control', 'no-cache');
     try {
         const { first_name, last_name, password } = request.body;
@@ -82,18 +91,22 @@ export const updateUser = async (request, response) => {
         if(!Object.values(request.body).every(val => {
             return typeof val === 'string' && val.trim() !== '' && val !== null;
         })){
+            logger.error("Error occured while updating user : Payload values should not be empty/null");
             setErrorResponse(400,"Payload values should not be empty/null",response);
         }
         else if(password && !passwordPattern.test(password)){
+            logger.error("Error occured while updating user : Password should be alphanumeric with special characters and at least 6 characters long");
             setErrorResponse(400,"Password should be alphanumeric with special characters and at least 6 characters long",response);
         } else {
             const allowedFields = ["first_name", "last_name", "password"];
             const invalidFields = Object.keys(request.body).filter(val => !allowedFields.includes(val));
             if (invalidFields.length > 0) {
+                logger.error(`Error occured while updating user : Invalid fields - ${invalidFields.join(', ')} to update`);
                 setErrorResponse(400, `Invalid fields - ${invalidFields.join(', ')} to update`, response);
             } else {
                 const auth = request.headers.authorization;
                 if (!auth || Object.keys(auth).length === 0) {
+                    logger.error(`Error occured while updating user : Authentication credentials required`);
                     setErrorResponse(400, "Authentication credentials required", response);
                 } else {
                     const authenticatedUser = await authentication(auth);
@@ -111,20 +124,24 @@ export const updateUser = async (request, response) => {
                         }
                         authenticatedUser.account_updated = new Date();
                         await authenticatedUser.save();
+                        logger.info("User updated successfully");
                         response.status(204).send();
                     } else {
+                        logger.error(`Error occured while updating user : Invalid credentials provided`);
                         setErrorResponse(401, "Invalid credentials provided", response);
                     }
                 }
             }
         }
     } catch (err) {
+        logger.error(`Error occured while updating user : Service Unavailable`);
         setErrorResponse(503, "Service Unavailable", response);
     }
 }
 
 export const methodCheck = async (request, response) => {
     response.set('Cache-Control', 'no-cache');
+    logger.error(`Error occured while updating user : Requested method not allowed`);
     response.status(405).send();
 }
 
