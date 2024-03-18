234403d51570f1096ce6bd31787186727ed9162f
Arpitha Sheshadri Bhat <144745793+arpithasheshadri@users.noreply.github.com>
2024-03-18 14:09:44 -0400

    modified logger to log severity

--- controllers/healthcheck-controller.js
+++ controllers/healthcheck-controller.js
@@ -7,23 +7,38 @@ export const healthCheck = async (request, response) => {
     response.set('Cache-Control', 'no-cache');
     try {
         if(Object.keys(request.body).length != 0 || Object.keys(request.query).length != 0){
-            logger.error("request body (or) request parameter should not be sent");
+            logger.error({
+                message: 'request body (or) request parameter should not be sent',
+                severity: 'INFO'
+              });
             response.status(400).send();
         } else {
             await checkConnection();
-            logger.info("Healthcheck successful");
-            logger.info("Healthcheck completed");
+            logger.info({
+                message: "Healthcheck successful",
+                severity: 'INFO'
+              });
+              logger.info({
+                message: "Healthcheck completed",
+                severity: 'INFO'
+              });
             response.status(200).send();
         }
     } catch (err) {
-        logger.error(`Healthcheck Failed : ${err}`);
+        logger.error({
+            message: `Healthcheck Failed : ${err}`,
+            severity: 'ERROR'
+          });
         setErrorResponse(503, err, response);
     }
 }
 
 export const methodCheck = async (request, response) => {
     response.set('Cache-Control', 'no-cache');
-    logger.error(`Healthcheck Failed : Requested method not allowed`);
+    logger.error({
+        message: `Healthcheck Failed : Requested method not allowed`,
+        severity: 'ERROR'
+      });
     response.status(405).send();
 }
 
