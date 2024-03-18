85dfe11fe47c4faadb455a272c78bf56049471fd
Arpitha Sheshadri Bhat <144745793+arpithasheshadri@users.noreply.github.com>
2024-03-17 23:48:45 -0400

    added code related to opsagent setup

---
+++ ops_agent_setup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# install ops agent command
+curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
+sudo bash add-google-cloud-ops-agent-repo.sh --also-install
+
+logging_config=$(cat <<EOF
+logging:
+  receivers:
+    my-app-receiver:
+      type: files
+      include_paths:
+        - /var/log/webapp.log
+      record_log_file_path: true
+  processors:
+    my-app-processor:
+      type: parse_json
+      time_key: time
+      time_format: "YYYY-MM-DDTHH:MM:SS.sssZ"
+  service:
+    pipelines:
+      default_pipeline:
+        receivers: [my-app-receiver]
+        processors: [my-app-processor]
+EOF
+)
+
+# Backup the original config file
+sudo cp /etc/google-cloud-ops-agent/config.yaml /etc/google-cloud-ops-agent/config.yaml.backup
+
+# Add the logging configuration to the config file
+echo "$logging_config" | sudo tee -a /etc/google-cloud-ops-agent/config.yaml > /dev/null
+
+
+sudo systemctl restart google-cloud-ops-agent
