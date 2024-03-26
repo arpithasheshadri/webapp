dba73945940cc30e47137df8903c6b7514d7aa0e
Arpitha Sheshadri Bhat <144745793+arpithasheshadri@users.noreply.github.com>
2024-03-26 13:59:56 -0400

    added controller to verify user

---
+++ controllers/verify-controller.js
@@ -0,0 +1,40 @@
+import { userVerification, setVerification } from "../services/verify-service.js";
+import { setResponse, setErrorResponse } from "./response-handler.js";
+import logger from "./logger.js";
+
+export const verifyUser = async (request, response) => {
+    const token = request.query.token;
+    logger.debug({
+        message: "User verification started",
+        severity: 'DEBUG'
+      });
+    response.set('Cache-Control', 'no-cache');
+    try {
+        const user = userVerification(token);
+        if(user){
+            const curDate = new Date.now();
+            if(curDate <= user.expiryTime){
+                const user = setVerification(user.email);
+                logger.info({
+                    message: "User verification successful",
+                    severity: 'INFO'
+                  });
+                response.status(200).send();
+            } else {
+                setErrorResponse(403,"Verification failed! Verification link has expired.",response);
+            }
+            
+        }
+        else {
+            setErrorResponse(400,"Broken link! Verification token does not exist.",response);
+        }
+      
+        
+    } catch (err) {
+        logger.error({
+            message: `Healthcheck Failed : ${err}`,
+            severity: 'ERROR'
+          });
+        setErrorResponse(503, err, response);
+    }
+}
