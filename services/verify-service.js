38154ce2f904872397fa9d80d72c5ad0b1f2c36e
Arpitha Sheshadri Bhat <144745793+arpithasheshadri@users.noreply.github.com>
2024-03-27 00:59:18 -0400

    added necessary log statements

--- services/verify-service.js
+++ services/verify-service.js
@@ -1,13 +1,9 @@
-import sequelize from "../db/sequelize.js";
-import User from "../models/User.js";
 import EmailTracking from "../models/EmailTracking.js";
 import { fetchUser } from "./user-service.js";
-import logger from '../controllers/logger.js';
+
 
 export const userVerification = async (token) => {
-    logger.info({message:`i am here token ${token}`,severity: 'INFO'});
     const user = await EmailTracking.findOne({ where: { token: token } });
-    logger.info({message:`i am here object retrieved ${user}`,severity: 'INFO'});
     return await user;
 }
 
